7bb83f3816715b51f446d22e56677ae6aeefce76
Aleksei Panchenko <nosmokingsurfer@gmail.com>
2020-02-27 15:57:16 +0300

    reading pictures using c++14 std::filesystem paths

--- helpers.h
+++ helpers.h
@@ -6,63 +6,22 @@
 #include <vector>
 #include <string>
 
-std::vector<std::string> load_test_2(void)
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
+
+std::vector<std::string> load_test(const std::string& path)
 {
   std::vector<std::string> result;
 
-  result.push_back("E:/visual/test_2/IMG_8830.jpg");
-  result.push_back("E:/visual/test_2/IMG_8785.jpg");
-  result.push_back("E:/visual/test_2/IMG_8786.jpg");
-  result.push_back("E:/visual/test_2/IMG_8787.jpg");
-  result.push_back("E:/visual/test_2/IMG_8788.jpg");
-  result.push_back("E:/visual/test_2/IMG_8789.jpg");
-  result.push_back("E:/visual/test_2/IMG_8790.jpg");
-  result.push_back("E:/visual/test_2/IMG_8791.jpg");
-  result.push_back("E:/visual/test_2/IMG_8792.jpg");
-  result.push_back("E:/visual/test_2/IMG_8793.jpg");
-  result.push_back("E:/visual/test_2/IMG_8794.jpg");
-  result.push_back("E:/visual/test_2/IMG_8795.jpg");
-  result.push_back("E:/visual/test_2/IMG_8796.jpg");
-  result.push_back("E:/visual/test_2/IMG_8797.jpg");
-  result.push_back("E:/visual/test_2/IMG_8798.jpg");
-  result.push_back("E:/visual/test_2/IMG_8799.jpg");
-  result.push_back("E:/visual/test_2/IMG_8800.jpg");
-  result.push_back("E:/visual/test_2/IMG_8801.jpg");
-  result.push_back("E:/visual/test_2/IMG_8802.jpg");
-  result.push_back("E:/visual/test_2/IMG_8803.jpg");
-  result.push_back("E:/visual/test_2/IMG_8804.jpg");
-  result.push_back("E:/visual/test_2/IMG_8805.jpg");
-  result.push_back("E:/visual/test_2/IMG_8806.jpg");
-  result.push_back("E:/visual/test_2/IMG_8807.jpg");
-  result.push_back("E:/visual/test_2/IMG_8808.jpg");
-  result.push_back("E:/visual/test_2/IMG_8809.jpg");
-  result.push_back("E:/visual/test_2/IMG_8810.jpg");
-  result.push_back("E:/visual/test_2/IMG_8811.jpg");
-  result.push_back("E:/visual/test_2/IMG_8812.jpg");
-  result.push_back("E:/visual/test_2/IMG_8813.jpg");
-  result.push_back("E:/visual/test_2/IMG_8814.jpg");
-  result.push_back("E:/visual/test_2/IMG_8815.jpg");
-  result.push_back("E:/visual/test_2/IMG_8816.jpg");
-  result.push_back("E:/visual/test_2/IMG_8817.jpg");
-  result.push_back("E:/visual/test_2/IMG_8818.jpg");
-  result.push_back("E:/visual/test_2/IMG_8819.jpg");
-  result.push_back("E:/visual/test_2/IMG_8820.jpg");
-  result.push_back("E:/visual/test_2/IMG_8821.jpg");
-  result.push_back("E:/visual/test_2/IMG_8822.jpg");
-  result.push_back("E:/visual/test_2/IMG_8823.jpg");
-  result.push_back("E:/visual/test_2/IMG_8824.jpg");
-  result.push_back("E:/visual/test_2/IMG_8825.jpg");
-  result.push_back("E:/visual/test_2/IMG_8826.jpg");
-  result.push_back("E:/visual/test_2/IMG_8827.jpg");
-  result.push_back("E:/visual/test_2/IMG_8828.jpg");
-  result.push_back("E:/visual/test_2/IMG_8829.jpg");
-
-  std::sort(result.begin(), result.end());
+  for (const auto& f : fs::directory_iterator(path))
+  {
+    std::cout << f.path() << std::endl;
+    result.push_back(f.path().string());
+  }
 
   return result;
 }
-
-
-
 
 #endif //HELPERS_2019
